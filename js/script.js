7a32dc42963a3b989dab0bd665147b4e09da8475
Jyotiraditya <jyotiradityapatra2@gmail.com>
2023-10-03 15:05:16 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// typing animation
+var typed=new Typed(".typing", {
+    strings:["Web Developer","Web Designer","Graphics designer"],
+    typeSpeed:100,
+    backspped:60,
+    loop:true
+})
+
+/*social media icon */
